049e22e68b4c799fdfca9cc26ef973e6365adbc5
Samiran2004 <samirans170@gmail.com>
2025-08-08 18:43:15 +0530

    add/Basic Setup for backend<Samiran>

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Hello World...");
+import express from 'express'
+
+const app = express();
+
+app.get('/health', (req, res) => {
+    return res.status(200).json({
+        status: 'Ok',
+        message: "Server is up and running..."
+    });
+});
+
+app.listen(8000, (err) => {
+    if (err) {
+        console.log("Error in server startup...");
+    } else {
+        console.log("Server running on port 8000");
+    }
+});
